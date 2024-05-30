de0566e2f6a243454aaf9c710ecbc3c06ccc5da5
JSE <julien.sebire@university-365.com>
2024-05-30 22:39:32 +0200

    update img nav

--- src/navigation.js
+++ src/navigation.js
@@ -176,7 +176,7 @@ export const footerData = {
     { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://youtube.com/@julienalchemist' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" url="src/assets/images/logo no background.png" alt="onWidget logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" [url("~/assets/images/logo no background.png")] alt="Julien Alchimist" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://www.youtube.com/@julienalchemist"> Julien Alchemist</a> · All rights reserved.
   `,
 };
